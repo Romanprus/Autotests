bb51d2b4681b8a2c70d884bfe139dbf7dbc3703a
roman <prusroman2@gmail.com>
2023-01-02 14:54:56 +0200

    comit

--- Framework/utilities/driver_factory.py
+++ Framework/utilities/driver_factory.py
@@ -1,11 +1,9 @@
 from selenium.webdriver import Chrome, Firefox, Edge
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.edge.service import Service
-from selenium.webdriver.firefox.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
+from selenium.webdriver.chrome.options import Options
 
 
 class DriverFactory:
@@ -14,10 +12,11 @@ class DriverFactory:
     EDGE = 3
 
     @staticmethod
-    def create_driver(driver_id: int, is_headless=False):
+    def create_driver(driver_id: int, headless=True):
         if int(driver_id) == DriverFactory.CHROME:
             chrome_options = Options()
-            if is_headless:
+            if headless:
+                chrome_options.add_argument("--headless")
                 chrome_options.add_argument("--no-sandbox")
                 chrome_options.add_argument('--allow-running-insecure-content')
                 chrome_options.add_argument('--disable-dev-shm-usage')
